a6dcc749df6abc9e751e77a21b60e322a3751bd7
BzRub <trabajofinalgradoruben@gmail.com>
2024-05-14 22:30:21 +0200

    Install firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJFeiqXkJxVU8SIwzFffvXx3RzPLEgCYg",
+    authDomain: "baebids.firebaseapp.com",
+    projectId: "baebids",
+    storageBucket: "baebids.appspot.com",
+    messagingSenderId: "465881448087",
+    appId: "1:465881448087:web:2062c42fd8a4b12f95f3d2"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
